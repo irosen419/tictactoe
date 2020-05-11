cb4006b837ef5d5757bac4c6346d9f9e5a358a94
irosen419 <irosen419@gmail.com>
2020-05-11 14:22:39 -0400

    Initial commit

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,4 +1,5 @@
-# this comment is to test git branches
+# another test
+
 
 # an array of all the possible winning combinations
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
