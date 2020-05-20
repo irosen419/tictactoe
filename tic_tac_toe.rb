3062d2a3e59f13091f04d488e9306e3f555557ea
irosen419 <61032240+irosen419@users.noreply.github.com>
2020-05-20 10:22:58 -0400

    Add files via upload

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,10 @@
 class TicTacToe
+
+  # initialize board
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+
   # an array of all the possible winning combinations
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
   CORNERS = [0, 2, 6, 8]
